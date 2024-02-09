4b69d664401007c3fdb0d26189f005bdb0f2df6d
alpha4vit <gurinovi4rm@gmail.com>
2024-02-09 22:14:57 +0300

    init

---
+++ electronicdiary.cpp
@@ -0,0 +1,14 @@
+#include "electronicdiary.h"
+#include "ui_electronicdiary.h"
+
+ElectronicDiary::ElectronicDiary(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::ElectronicDiary)
+{
+    ui->setupUi(this);
+}
+
+ElectronicDiary::~ElectronicDiary()
+{
+    delete ui;
+}
---
+++ electronicdiary.h
@@ -0,0 +1,23 @@
+#ifndef ELECTRONICDIARY_H
+#define ELECTRONICDIARY_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class ElectronicDiary;
+}
+QT_END_NAMESPACE
+
+class ElectronicDiary : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ElectronicDiary(QWidget *parent = nullptr);
+    ~ElectronicDiary();
+
+private:
+    Ui::ElectronicDiary *ui;
+};
+#endif // ELECTRONICDIARY_H
